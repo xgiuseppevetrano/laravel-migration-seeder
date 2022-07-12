83765cf7259d897b9c685a54da829e4c9c36f226
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-07-12 16:37:12 +0200

    first part add data table

---
+++ database/seeds/TrainsTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Train;
+use Faker\Generator as Faker;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 50; $i++) { 
+            $newTrain = new Train();
+
+            $newTrain->company = $faker->word();
+            $newTrain->departure_station = $faker->city();
+            $newTrain->arrival_station = $faker->city();
+            $newTrain->departure_time = $faker->time();
+            $newTrain->arrival_time = $faker->time();
+            $newTrain->departure_date = $faker->date();
+            $newTrain->arrival_date = $faker->date();
+            $newTrain->train_code = $faker->randomNumber(5, true);
+            $newTrain->carriages = rand(1, 100);
+
+            $newTrain->save();
+        }
+    }
+}
