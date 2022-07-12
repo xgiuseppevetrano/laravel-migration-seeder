d323f9a43a776519ef4891504960d54d61ced88f
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-07-12 16:15:42 +0200

    create table

--- database/migrations/2022_07_12_134859_create_trains_table.php
+++ database/migrations/2022_07_12_134859_create_trains_table.php
@@ -15,7 +15,17 @@ class CreateTrainsTable extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
-
+            $table->string('company', 30);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->dateTime('departure_time', 0);
+            $table->dateTime('arrival_time', 0);
+            $table->date('departure_date');
+            $table->date('arrival_date');
+            $table->string('train_code', 5);
+            $table->unsignedTinyInteger('carriages');
+            $table->boolean('on_time')->default(true);
+            $table->boolean('is_deleted')->default(false);
             $table->timestamps();
         });
     }
